152c72d1f31f2c50c44797994291cd423774a5a0
dharlan <90869420+LULAPT@users.noreply.github.com>
2025-03-28 09:59:42 -0300

    typing cursor fix

--- js/typing.js
+++ js/typing.js
@@ -1,28 +1,29 @@
 const texts = document.getElementById('typingTexts').innerHTML.split(' &lt;&gt; ');
 const textElement = document.getElementById('typingText');
+const cursorChar = "|"; // Cursor character added here
 let currentTextIndex = 0;
 
 function typeWriter(text, i) {
     if (i < text.length) {
-        textElement.textContent += text.charAt(i);
+        textElement.textContent = text.substring(0, i + 1) + cursorChar;
         i++;
         setTimeout(function() {
             typeWriter(text, i);
-        }, 300);
+        }, 185);
     } else {
         setTimeout(function() {
             eraseText(text);
-        }, 1000);
+        }, 1000); // Added a 2-second delay before erasing
     }
 }
 
 function eraseText(text) {
     let length = text.length;
     if (length > 0) {
-        textElement.textContent = text.substring(0, length - 1);
+        textElement.textContent = text.substring(0, length - 1) + cursorChar;
         setTimeout(function() {
             eraseText(text.substring(0, length - 1));
-        }, 50);
+        }, 40);
     } else {
         currentTextIndex = (currentTextIndex + 1) % texts.length;
         setTimeout(function() {
