fc73453f3cb7d32b31164ee218370c4875673b61
dharlan <90869420+LULAPT@users.noreply.github.com>
2025-05-18 06:47:51 -0300

    oi

--- js/player.js
+++ js/player.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
             audio.play();
             playIcon.style.display = 'none';
             pauseIcon.style.display = 'block';
-        }, 1800);
+        }, 2300);
     }
 
     // Adiciona evento de clique no site para habilitar o autoplay
@@ -125,7 +125,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const audioPlayer = document.querySelector('.audioPlayer');
     const volumeSlider = document.querySelector('.volume-slider');
     const volumeIcon = document.querySelector('.volume-icon');
-    let lastVolume = 0.64; // Volume inicial definido para 50%
+    let lastVolume = 0.10; // Volume inicial definido para 50%
 
     // Definir volume inicial
     audioPlayer.volume = lastVolume;
