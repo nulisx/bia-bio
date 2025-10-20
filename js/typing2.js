5516f2ced6c56331dd6b1369c1f2200bae6f3b05
nulisx <edrmebitch@fbi.one>
2025-10-20 19:24:03 -0400

    Update typing2.js

--- js/typing2.js
+++ js/typing2.js
@@ -3,33 +3,41 @@ const textElement = document.getElementById('typingText');
 const cursorChar = "|";
 let currentTextIndex = 0;
 
-function typeWriter(text, i) {
+function typeWriter(text, i = 0) {
+    // if the text contains HTML tags (like <a>), dont type it char by char
+    if (/<[a-z][\s\S]*>/i.test(text)) {
+        textElement.innerHTML = text + cursorChar;
+        setTimeout(() => eraseText(text), 1200);
+        return;
+    }
+
     if (i <= text.length) {
-        if (/<[a-z][\s\S]*>/i.test(text)) {
-            textElement.innerHTML = text.substring(0, i) + cursorChar;
-        } else {
-            textElement.textContent = text.substring(0, i) + cursorChar;
-        }
-        i++;
-        setTimeout(() => typeWriter(text, i), 92.5);
+        textElement.textContent = text.substring(0, i) + cursorChar;
+        setTimeout(() => typeWriter(text, i + 1), 92.5);
     } else {
         setTimeout(() => eraseText(text), 1000);
     }
 }
 
 function eraseText(text) {
-    let length = text.length;
-    if (length > 0) {
-        if (/<[a-z][\s\S]*>/i.test(text)) {
-            textElement.innerHTML = text.substring(0, length - 1) + cursorChar;
-        } else {
-            textElement.textContent = text.substring(0, length - 1) + cursorChar;
-        }
-        setTimeout(() => eraseText(text.substring(0, length - 1)), 40);
-    } else {
-        currentTextIndex = (currentTextIndex + 1) % texts.length;
-        setTimeout(() => typeWriter(texts[currentTextIndex], 0), 500);
+    if (/<[a-z][\s\S]*>/i.test(text)) {
+        // if it was/is a link or HTML chunk, removes it instantly
+        textElement.innerHTML = cursorChar;
+        next();
+        return;
     }
+
+    if (text.length > 0) {
+        textElement.textContent = text.substring(0, text.length - 1) + cursorChar;
+        setTimeout(() => eraseText(text.substring(0, text.length - 1)), 40);
+    } else {
+        next();
+    }
+}
+
+function next() {
+    currentTextIndex = (currentTextIndex + 1) % texts.length;
+    setTimeout(() => typeWriter(texts[currentTextIndex], 0), 500);
 }
 
 typeWriter(texts[currentTextIndex], 0);
